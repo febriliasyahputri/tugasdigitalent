ba28152b781fe004b63edd1cf4ee0ad275a0aeda
Febrilia Syahputri <febriliasyahputri@gmail.com>
2018-11-13 21:03:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <title> Hello World!</title>
+
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="assets/css/bootstrap.css"/>
+      <link rel="stylesheet" href="assets/css/font-awesome.css"/>
+      <link href='assets/img/icon.png' rel='shortcut icon'>
+      <!-- Java Script -->
+      <script type="text/javascript" src="assets/js/jquery-2.1.4.js"></script>
+      <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+      <style>
+      body{
+      margin:0;
+      }
+      .jumbotron {
+      background-color: #68b6b8;
+      background-size: 50%;
+      color: #516887;
+      padding: 50px 30px;
+      height: 250px;
+      margin-top: 50px;
+      }
+      .navbar-default {
+        background: #516887;
+        font-size: 20px;
+      }
+      .no-border {
+        border: 0;
+      }
+      .no-radius {
+        border-radius: 0px;
+      }
+      .circle {
+        border-radius: 50%;
+      }
+      .card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+   
+      }
+
+      .card2 {
+    box-shadow: 2 4px 8px 0 rgba(0,0,0,0.2);
+   
+      }
+
+      .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+      }
+
+      .card-p{
+        padding-top: 15px;
+        font-size: 20px;
+        color: #fff;
+        font-family: arial;
+        padding-left: 20px;
+      }
+
+      img {
+          border-radius: 5px 5px 0 0;
+      }
+
+      .container {
+          padding: 2px 16px;
+      }
+      .shadow {
+        box-shadow: 3px 1px 2px 2px rgba(0, 0, 0, 0.2);
+      }
+      .navbar-default .navbar-brand {
+        color: white;
+        font-size: 20px;
+        letter-spacing: 2px;
+      }
+      .navbar-default .navbar-nav > li > a {
+        color: white;
+      }
+      .navbar-default .navbar-nav > li > a:hover {
+        color: #fff;
+        background: #516887;
+      }
+      .navbar-default .navbar-nav > .open > a, .navbar-default .navbar-nav > .open > a:hover, .navbar-default .navbar-nav > .open > a:focus {
+        color: #fff;
+        background: #516887;
+      }
+      .navbar-default .navbar-brand:hover, .navbar-default .navbar-brand:focus {
+      color: #fff;
+      background-color: transparent;
+      }
+      .dropdown-menu > li > a {
+        color :#516887;
+        font-size: 20px;
+      }
+      .container-fluid {
+      padding: 7px 50px;
+      }
+      .bg-grey {
+      background-color: #f6f6f6;
+      }
+      .logo-small {
+      color: #fff;
+      font-size: 50px;
+      }
+      .logo {
+      color: #0a9;
+      font-size: 200px;
+      }
+      @media screen and (max-width: 768px) {
+      .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+      }
+      }
+      </style>
+ </head>
+ <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+   <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top no-border no-radius">
+      <div class="container-fluid">
+         <div class="navbar-header">
+         </div>
+      </div><!--/.container-fluid -->
+    </nav>
+
+<div class="jumbotron ">
+  <center><h1 class="display-4">Hello World!</h1>
+  <br></center>
+  <center><p class="lead"></p>
+  <hr class="my-6"></center>
+</div>
+
+     <div class="container-fluid">
+     <div class="row">
+
+        <div class="col-sm-4">
+
+        <div class="card" style="background-color: #708090; height: 52px;">
+        <div class="container">
+        <p style="font-size: 20px; color: #fff; padding-left: 120px; padding-top: 10px;"></p>
+          </div>
+        </div>
+         <br>
+        <div class="card" style="background-color: #b54460; height: 52px;">
+          <div class="container">
+            <p class="card-p"></p> 
+          </div>
+        </div>
+        <br>
+        <div class="card" style="background-color: #9b9cd6; height: 52px;">
+          <div class="container">
+            <p class="card-p"></p>  
+          </div>
+        </div>
+        <br>
+        <div class="card" style="background-color: #79b4c1; height: 52px;">
+          <div class="container">
+            <p class="card-p"></p> 
+          </div>
+        </div>
+        <br>
+         <div class="card" style="background-color: #aaaaaa; height: 52px;">
+        <div class="container">
+        <p style="font-size: 20px; color: #fff; padding-left: 80px; padding-top: 10px;"></p>
+          </div>
+        </div>
+        </div>
+    
+      <div class="col-sm-8">
+      <div class="card" style=" padding-top: 5px;">
+          <center><h2 style="color: #516887;">Hello World</h2></center>
+          <hr style="color:#493e66;">
+            <p style="padding-left: 35px; text-align: justify; font-size: 15px; padding-right: 35px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My name is Febrilia Syahputri.I live in Bogor, Indonesia. At this time I am 17 years old. I have graduated from SMKN 1 CIOMAS majoring in software engineering. And I am currently participating in digital talent scholarship 2018. I am very happy to be able to attend this training,have new friends,and they are good. as well as new insights in the world of information technology,and it is very broad and there are many things that I do not know. <br><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I am very grateful to the lecturers who taught me about cloud computing, about things I did not know before.I hope I will continue to study and I am ready to follow technological developments in the world.
+              </p> 
+              <br>
+              <br>
+          </div>
+      </div>
+  </div>
+</div>
+<br>
+<div class="card2" style="background-color:  #708090;">
+        <div class="container">
+          </div>
+        </div>
+        <br>
+<div class="card2" style="background-color:  #F08080;">
+        <div class="container">
+          </div>
+        </div>
+        <br>
+<div class="card2" style="background-color:  #20B2AA">
+        <div class="container">
+          </div>
+        </div>
+<br>
+<div  style="color: #516887; border-top: 3px solid #516887; ">
+  </div>
+
+ </body>
+</html>
